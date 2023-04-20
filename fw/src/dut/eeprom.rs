83453722389077243a4381d65f202b810f36ba22
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2023-04-19 22:34:25 -0400

    Use Atomic<T> instead of static mut for eeprom test status

--- fw/src/dut/eeprom.rs
+++ fw/src/dut/eeprom.rs
@@ -1,10 +1,19 @@
+use atomic::Atomic;
+use ccmn_eol_shared::atomics::*;
+
 use crate::libeeprom;
 use crate::opencan::tx::*;
 
 static EEPROM_ADDR: u16 = 0xDEAD;
 static TEST_DATA: [u8; 8] = [68, 69, 65, 68, 66, 69, 69, 70];
 
-static mut EEPROM_TEST_STATUS: Option<bool> = None;
+struct _G {
+    test_status: Atomic<Option<bool>>,
+}
+
+static _G: _G = _G {
+    test_status: Atomic::<_>::new(None)
+};
 
 /// Write and read from the eeprom
 pub fn eeprom_eol_test() {
@@ -20,21 +29,16 @@ pub fn eeprom_eol_test() {
 
     if buff.eq(&TEST_DATA) {
         println!("eeprom data: {:?}", std::str::from_utf8(&buff).unwrap());
-        unsafe {
-            EEPROM_TEST_STATUS = Some(true);
-        }
+        glo_w!(test_status, Some(true));
     } else {
         println!("Warning: eeprom is not working!");
-
-        unsafe {
-            EEPROM_TEST_STATUS = Some(false);
-        }
+        glo_w!(test_status, Some(false));
     }
 }
 
 #[no_mangle]
 extern "C" fn CANTX_populate_DUT_EepromTestStatus(m: &mut CAN_Message_DUT_EepromTestStatus) {
-    m.DUT_testStatus = match unsafe { EEPROM_TEST_STATUS } {
+    m.DUT_testStatus = match glo!(test_status) {
         Some(true) => CAN_DUT_testStatus::CAN_DUT_TESTSTATUS_TEST_PASSED,
         Some(false) => CAN_DUT_testStatus::CAN_DUT_TESTSTATUS_TEST_FAILED,
         None => CAN_DUT_testStatus::CAN_DUT_TESTSTATUS_NOT_RUN,
